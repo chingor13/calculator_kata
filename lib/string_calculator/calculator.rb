a8ca773728b841c622a78673683671de00d1e93f
Jeff Ching <jching@avvo.com>
2015-06-02 12:14:50 -0700

    ignore numbers greater than 1000

--- lib/string_calculator/calculator.rb
+++ lib/string_calculator/calculator.rb
@@ -14,11 +14,17 @@ module StringCalculator
         numbers = lines[1..-1].join("\n")
       end
 
+      # number validator
       real_numbers = numbers.split(delimiter).map(&:to_i)
       negatives = real_numbers.select do |num|
         num < 0
       end
       raise ArgumentError, "negatives not allowed: #{negatives.join(",")}" if negatives.length > 0
+
+      # number rejector
+      real_numbers.reject! do |num|
+        num > 1000
+      end
 
       real_numbers.inject(0, &:+)
     end
